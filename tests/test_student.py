0b9c1c4c0d7c56f2596bfcf4e085f02880c62acf
jacy <jacyyang04@gmail.com>
2021-10-01 14:31:22 -0700

    add new tests, asserts and modify student class

--- tests/test_student.py
+++ tests/test_student.py
@@ -11,6 +11,17 @@ def test_init():
     assert ada.level == level
     assert ada.courses == courses
 
+def test_init_empty_list():
+    name = "Ada Lovelace"
+    level = "sophomore"
+    courses = []
+
+    ada = Student(name, level, courses)
+
+    assert ada.name == name
+    assert ada.level == level
+    assert ada.courses == []
+
 def test_add_class():
     new_class = 'Intro to Feminism'
     charles = Student("Charles Babbage", "senior", ["mechanical engineering"])
@@ -42,3 +53,7 @@ def test_get_student_with_more_classes():
     )
 
     # TODO: write assertions
+
+    assert get_student_with_more_classes(ada, charles) == ada
+    assert get_student_with_more_classes(charles, ada) == ada
+
